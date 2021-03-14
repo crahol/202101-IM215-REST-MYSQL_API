048056cd8954209fe598ec7794d06c4daefe9d0b
crahol <raholchaitali20@gmail.com>
2021-03-14 17:46:20 -0400

    Express Hello world end connection

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response) => {
-	console.log('request received');
-	response.json({msg:'Welcome'});
+	//console.log('request received');
+	//response.json({msg:'Welcome'});
+	response.end();
 })
 app.listen(3333, () => {
 	console.log("The server is up and listening on port 3333")
