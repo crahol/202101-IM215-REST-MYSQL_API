7d2a34fdbc8e1014883a1f0efff22d5936efed9a
crahol <raholchaitali20@gmail.com>
2021-03-14 17:40:27 -0400

    Express Hello World

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi ');
+const express = require('express');
+const app = express();
+
+app.get('/',(request,response) => {
+	console.log('request received');
+	response.json({msg:'Welcome'});
+})
+app.listen(3333, () => {
+	console.log("The server is up and listening on port 3333")
+})
